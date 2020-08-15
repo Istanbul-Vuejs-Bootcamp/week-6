bd2ac6684510bc86b2f0bc0426b4d6e65e73f495
Mustafa Demir <mustafa.demir@kboxglobal.com>
2020-08-15 13:37:04 +0300

    week-6

---
+++ user-auth-project/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+  const routes = [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/User/Profile.vue')
+    }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ user-auth-project/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        HOME PAGE
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
